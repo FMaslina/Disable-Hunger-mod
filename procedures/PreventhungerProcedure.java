517181d8f83c6672bb03c0bc3aac3abc9307d7f7
FMaslina <FMaslina@yandex.ru>
2025-09-13 19:44:28 +0700

    Initial

---
+++ procedures/PreventhungerProcedure.java
@@ -0,0 +1,39 @@
+package net.mcreator.disablehunger.procedures;
+
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.eventbus.api.Event;
+import net.minecraftforge.event.TickEvent;
+
+import net.minecraft.world.level.LevelAccessor;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.entity.Entity;
+
+import javax.annotation.Nullable;
+
+import java.util.ArrayList;
+
+@Mod.EventBusSubscriber
+public class PreventhungerProcedure {
+	@SubscribeEvent
+	public static void onPlayerTick(TickEvent.PlayerTickEvent event) {
+		if (event.phase == TickEvent.Phase.END) {
+			execute(event, event.player.level(), event.player);
+		}
+	}
+
+	public static void execute(LevelAccessor world, Entity entity) {
+		execute(null, world, entity);
+	}
+
+	private static void execute(@Nullable Event event, LevelAccessor world, Entity entity) {
+		if (entity == null)
+			return;
+		if (world.dayTime() % 100 == 20) {
+			for (Entity entityiterator : new ArrayList<>(world.players())) {
+				if (entity instanceof Player _player)
+					_player.getFoodData().setFoodLevel(20);
+			}
+		}
+	}
+}
